68b40841380a3174b63a5048a80e44d475c0ac73
ncip48 <mbahcip00@gmail.com>
2020-10-13 10:52:34 +0700

    remove gitignore files

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use("/api/v1/", router);
 
 //define port number
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 //listen the server
 app.listen(port, () => console.log(`Server running in port ${port}`));
