6986101f7c1bb3c8a08a8baacbd00e4f2e4e7d6f
ncip48 <mbahcip00@gmail.com>
2023-01-17 22:03:48 +0700

    csr

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const httpsServer = https.createServer(
   {
     key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
     cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+    ca: fs.readFileSync(path.join(__dirname, "cert", "csr.pem")),
   },
   app
 );
